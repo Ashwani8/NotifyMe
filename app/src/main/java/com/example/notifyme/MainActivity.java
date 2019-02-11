f02b50498ff4d2cd5fd54c17d3fe048b9c89ed1c
Ashwani Kumar <akumar@monmouthcollege.edu>
2019-02-10 18:19:33 -0600

    Create a Notification Channel

--- app/src/main/java/com/example/notifyme/MainActivity.java
+++ app/src/main/java/com/example/notifyme/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.notifyme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -27,6 +30,18 @@ public class MainActivity extends AppCompatActivity {
     }
     public void createNotificationChannel(){
         mNotifyManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        if(android.os.Build.VERSION.SDK_INT >=
+                android.os.Build.VERSION_CODES.O){
+            // Create a Notification Channel
+            NotificationChannel notificationChannel = new NotificationChannel(PRIMARY_CHANNEL_ID,
+                    "Mascot Notification", NotificationManager.IMPORTANCE_HIGH);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription("Notification from Mascot");
+            mNotifyManager.createNotificationChannel(notificationChannel);
+        }
     }
 
     // sends notification to user
